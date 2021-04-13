16c3c127f8415452a81a4a42dcf1b98e11bec308
ÉN <email@gmail.com>
2021-04-13 21:45:48 +0200

    Order_class by Norbi

---
+++ src/main/java/hu/unideb/inf/Order.java
@@ -0,0 +1,49 @@
+package hu.unideb.inf;
+
+import java.util.Date;
+
+public class Order {
+    public String ID;
+    public String FILM;
+    public String ULO_HELY;
+    public Date DATE ;
+    public String MEAL;
+
+    public String getID() {
+        return ID;
+    }
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    public String getFILM() {
+        return FILM;
+    }
+
+    public void setFILM(String FILM) {
+        this.FILM = FILM;
+    }
+
+    public String getULO_HELY() {
+        return ULO_HELY;
+    }
+
+    public void setULO_HELY(String ULO_HELY) {
+        this.ULO_HELY = ULO_HELY;
+    }
+
+    public Date getDATE() {
+        return DATE;
+    }
+
+    public void setDATE(Date DATE) {
+        this.DATE = DATE;
+    }
+
+    public String getMEAL() {
+        return MEAL;
+    }
+
+    public void setMEAL(String MEAL) {
+        this.MEAL = MEAL;
+    }
+}
