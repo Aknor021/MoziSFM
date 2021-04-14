ee2946f1b0b31fea1ad1277c19deb26d3980d663
Bereczki Norbert <aknor021@gmail.com>
2021-04-14 17:38:23 +0200

    Adatbázis txt + csv

--- src/main/java/hu/unideb/inf/FilmSelectController.java
+++ src/main/java/hu/unideb/inf/FilmSelectController.java
@@ -2,9 +2,12 @@ package hu.unideb.inf;
 
 
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,7 +17,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class FilmSelectController implements Initializable {
+import javax.xml.crypto.Data;
+
+public class FilmSelectController    {
+
+    String film;
+
+    public FilmSelectController() throws IOException {
+    }
+
 
     @FXML
     void BackButtonPushed(ActionEvent event) throws IOException {
@@ -25,8 +36,48 @@ public class FilmSelectController implements Initializable {
         window.show();
     }
 
-    @FXML
+
+/*    @FXML
     void NextButtonPushed(ActionEvent event) throws IOException {
+        Parent Next_to_Time = FXMLLoader.load(getClass().getResource("/fxml/Time_Select.fxml") );
+        Scene Time_scene = new Scene(Next_to_Time);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(Time_scene);
+        window.show();
+    }
+    */
+
+    @FXML
+    void film1(ActionEvent event) throws IOException {
+        film = "Free Guy";
+        film_save(film);
+        Parent Next_to_Time = FXMLLoader.load(getClass().getResource("/fxml/Time_Select.fxml") );
+        Scene Time_scene = new Scene(Next_to_Time);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(Time_scene);
+        window.show();
+    }
+    @FXML
+    void film2(ActionEvent event) throws IOException {
+        film_save("Fekete özvegy");
+        Parent Next_to_Time = FXMLLoader.load(getClass().getResource("/fxml/Time_Select.fxml") );
+        Scene Time_scene = new Scene(Next_to_Time);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(Time_scene);
+        window.show();
+    }
+    @FXML
+    void film3(ActionEvent event) throws IOException {
+        film_save("Lelki terror");
+        Parent Next_to_Time = FXMLLoader.load(getClass().getResource("/fxml/Time_Select.fxml") );
+        Scene Time_scene = new Scene(Next_to_Time);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(Time_scene);
+        window.show();
+    }
+    @FXML
+    void film4(ActionEvent event) throws IOException {
+        film_save("film4");
         Parent Next_to_Time = FXMLLoader.load(getClass().getResource("/fxml/Time_Select.fxml") );
         Scene Time_scene = new Scene(Next_to_Time);
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -36,11 +87,23 @@ public class FilmSelectController implements Initializable {
 
 
 
+    void film_save(String film) throws IOException {
+        try {
+            FileWriter myWriter = new FileWriter("Current_order.txt");
+            myWriter.write(film);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
 
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
     }
+
+
+
+
+
+
+
 }
