5cffc8f85ade36ab857f22929d6e5b0741c8c105
Tomi <melindath16@gmail.com>
2021-05-10 20:31:44 +0200

    sorszám label implementálása

--- src/main/java/hu/unideb/inf/ENDSceneController.java
+++ src/main/java/hu/unideb/inf/ENDSceneController.java
@@ -3,14 +3,23 @@ package hu.unideb.inf;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
-import java.util.Objects;
 
-public class ENDSceneController {
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ENDSceneController implements Initializable {
 
     @FXML
     void NextButtonPushed(ActionEvent event) throws IOException {
@@ -20,5 +29,24 @@ public class ENDSceneController {
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(Menu_Scene);
         window.show();
+    }
+
+    @FXML
+    public Label sorszam;
+
+    public List<Order> o_list = new ArrayList<>();
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        OrderDAO oDAO = new JpaOrderDAO();
+
+        o_list = oDAO.getOrders();
+        Order order = o_list.get(o_list.size() - 1);
+        sorszam.setText(String.valueOf(order.getId()));
+
+        try {
+            oDAO.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
